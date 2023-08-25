ddac735ad4ed03e98433c64aa2777bd9ac25dfca
evlic <evlicn.ok@gmail.com>
2023-08-25 11:09:37 +0800

    add case

--- main.go
+++ main.go
@@ -1,7 +1,168 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 func main() {
 	fmt.Println("vim-go")
+}
+
+type Record struct {
+	ID          string `json:"id" db:"id"`
+	From        string `json:"from" db:"from"`
+	To          string `json:"to" db:"to"`
+	Sender      string `json:"sender" db:"sender"`
+	SenderIP    string `json:"sender_ip" db:"sender_ip"`
+	Receiver    string `json:"receiver" db:"receiver"`
+	Title       string `json:"title" db:"title"`
+	ContentType string `json:"content_type" db:"content_type"`
+	Content     string `json:"content" db:"content"`
+	AlertType   string `json:"channel" db:"alert_type"`
+	EventType   string `json:"type" db:"event_type"`
+	State       string `json:"state" db:"state"`
+	StateMsg    string `json:"state_msg" db:"state_msg"`
+	ReadOnly
+}
+
+type ReadOnly struct {
+	CreatedAt time.Time      `json:"created_at" gorm:"created_at"` // 生成日期
+	DeletedAt gorm.DeletedAt `json:"-" gorm:"deleted_at"`
+}
+
+func (m *ReadOnly) Create() {
+	m.CreatedAt = time.Now()
+}
+
+func RecordTableName() string {
+	return "alert_record"
+}
+
+func (Record) TableName() string {
+	return RecordTableName()
+}
+
+type dao struct {
+	orm *gorm.DB
+}
+
+const (
+	QueryListBufferSize = 8
+	QueryListMaximum = 10000
+)
+
+var TimeInterval = &struct {
+	Begin, End string
+}{
+	Begin: "begin",
+	End:   "end",
+}
+
+var ReadOnlyMapping = &struct {
+	CreatedAt, DeletedAt string
+}{
+	CreatedAt: "created_at",
+	DeletedAt: "deleted_at",
+}
+
+var RecordEnum = struct {
+	Channel struct {
+		Feishu, Wechat, Dingtalk, Mail string
+	}
+	State struct {
+		Success, Failed string
+	}
+}{
+	Channel: struct {
+		Feishu, Wechat, Dingtalk, Mail string
+	}{
+		Feishu:   "feishu",
+		Wechat:   "wechat",
+		Dingtalk: "dingtalk",
+		Mail:     "mail",
+	},
+	State: struct {
+		Success, Failed string
+	}{
+		Success: "ok",
+		Failed:  "error",
+	},
+}
+
+var ro = ReadOnlyMapping
+
+func (d *dao) ListRecord(c context.Context, inFilter, search, inTime map[string]any, sort string, limit, offset int) (res []*Record, err error) {
+	tx := d.orm.WithContext(c).Table(RecordTableName())
+	res = make([]*Record, 0, QueryListBufferSize)
+
+	if len(sort) > 4 {
+		tx = tx.Order(sort)
+	}
+
+	if inTime != nil {
+		interval := TimeInterval
+		tx = tx.Where(
+			"date("+ro.CreatedAt+") between date(?) and date(?)",
+			inTime[interval.Begin],
+			inTime[interval.End],
+		)
+	}
+
+	if search != nil {
+		txOr := tx
+		for filed, v := range search {
+			txOr = txOr.Or(filed+" like ?", v)
+		}
+		tx.Where(txOr)
+	}
+
+	tx = tx.Where(inFilter)
+
+	var total int64
+	err = tx.Count(&total).Error
+	if limit > 0 && offset > -1 {
+		tx = tx.Limit(limit).Offset(offset)
+	} else {
+		tx = tx.Limit(QueryListMaximum)
+	}
+
+	err = tx.Find(&res).Error
+	return
+}
+
+func (d *dao) CountRecord(c context.Context, filter, or, time map[string]any) (total int64, err error) {
+	tx := d.orm.WithContext(c).Table(RecordTableName())
+	if time != nil {
+		interval := TimeInterval
+		tx = tx.Where(
+			"date("+ro.CreatedAt+") between date(?) and date(?)",
+			time[interval.Begin],
+			time[interval.End],
+		)
+	}
+
+	filedCnt := 0
+	for filed, v := range or {
+		if filedCnt == 0 {
+			tx = tx.Where(filed+" like ?", v)
+			filedCnt++
+			continue
+		}
+		tx = tx.Or(filed+" like ?", v)
+		filedCnt++
+	}
+
+	tx = tx.Where(filter).Where("deleted_at is null")
+
+	err = tx.Count(&total).Error
+	return
+}
+
+func (d *dao) CreateRecord(c context.Context, r *Record) (err error) {
+	err = d.orm.WithContext(c).Create(r).Error
+	return
 }
--- main_test.go
+++ main_test.go
@@ -1,7 +1,17 @@
 package main
 
 import (
+	"context"
+	"os"
 	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"github.com/google/uuid"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 // GORM_REPO: https://github.com/go-gorm/gorm.git
@@ -9,12 +19,173 @@ import (
 // TEST_DRIVERS: sqlite, mysql, postgres, sqlserver
 
 func TestGORM(t *testing.T) {
-	user := User{Name: "jinzhu"}
+	TestBySQLite(t)
+}
 
-	DB.Create(&user)
+const (
+	dsn = ":memory:"
+	//logEnable = false
+	//logEnable = true
+)
 
-	var result User
-	if err := DB.First(&result, user.ID).Error; err != nil {
-		t.Errorf("Failed, got error: %v", err)
+var (
+	c = context.TODO()
+	d *dao
+	r *Record
+
+	stateEnum   = RecordEnum.State
+	stateAll    = []string{stateEnum.Success, stateEnum.Failed}
+	channelEnum = RecordEnum.Channel
+	channelAll  = []string{
+		channelEnum.Dingtalk,
+		channelEnum.Feishu,
+		channelEnum.Wechat,
+		channelEnum.Mail,
+	}
+)
+
+func TestMain(m *testing.M) {
+	testDB, err := gorm.Open(sqlite.Open(dsn))
+
+	if err != nil {
+		panic(err)
+	}
+
+	//testDBLog(testDB, logEnable)
+    testDBLogOn = func () {
+        testDBLog(testDB, true)
+    }
+    testDBLogOff = func () {
+        testDBLog(testDB, false)
+    }
+
+	d = &dao{testDB}
+	if err = testDB.AutoMigrate(r); err != nil {
+		panic(err)
+	}
+	os.Exit(m.Run())
+}
+
+func getRecord(i int) *Record {
+	rod := &Record{
+		ID:          uuid.New().String(),
+		To:          uuid.New().String(),
+		Sender:      uuid.New().String(),
+		Receiver:    uuid.New().String(),
+		Title:       uuid.New().String(),
+		ContentType: uuid.New().String(),
+		Content:     uuid.New().String(),
+		AlertType:   channelAll[i%len(channelAll)],
+		EventType:   uuid.New().String(),
+		SenderIP:    uuid.New().String(),
+		State:       stateAll[i%len(stateAll)],
+		StateMsg:    uuid.New().String(),
+	}
+	rod.Create()
+	return rod
+}
+
+const (
+	n = 100
+)
+
+func TestBySQLite(t *testing.T) {
+    //testDBLogOn()
+
+	Convey("insert", t, func() {
+       //testDBLogOff()
+		for i := 0; i < n; i++ {
+			r = getRecord(i)
+			err := d.CreateRecord(c, r)
+			So(err, ShouldBeNil)
+		}
+	})
+    testDBLogOn()
+	query := map[string]queryParam{
+		// "with-time":    getParamWithTimeLimit(),
+		// "filter-state": getParamWithState(),
+		// "sort-time":    getParamWithSortByTime(),
+		"search-x":     getParamWithSearch(),
+	}
+	Convey("query", t, func() {
+		for key, v := range query {
+			Convey("Testing " + key, func() {
+				cnt, err := d.CountRecord(c, v.filter, v.or, v.time)
+				So(err, ShouldBeNil)
+
+				list, err := d.ListRecord(c, v.filter, v.or, v.time, v.sort, v.page, v.size)
+                t.Log("Testing " + key, cnt, len(list), v.expectation)
+				So(err, ShouldBeNil)
+				So(cnt, ShouldEqual, len(list))
+				So(cnt, ShouldEqual, v.expectation)
+			})
+		}
+	})
+}
+
+var (
+	testDBLogOn  = func() {}
+	testDBLogOff = func() {}
+)
+
+func testDBLog(testDB *gorm.DB, enable bool) {
+	if enable {
+		testDB.Logger = logger.Default.LogMode(logger.Info)
+		return
+	}
+	testDB.Logger = logger.Default.LogMode(logger.Silent)
+}
+
+
+type queryParam struct {
+	filter map[string]any
+	or     map[string]any
+	time   map[string]any
+	sort   string
+	page   int
+	size   int
+
+	expectation int
+}
+
+func getParamWithTimeLimit() queryParam {
+	now := time.Now()
+	return queryParam{
+		time: map[string]any{
+			TimeInterval.Begin: now.Add(time.Hour * -24),
+			TimeInterval.End:   now,
+		},
+		expectation: n,
+	}
+}
+
+func getParamWithSortByTime() queryParam {
+	//now := time.Now()
+	return queryParam{
+		sort:        "+" + ro.CreatedAt,
+		expectation: n,
+	}
+
+}
+
+func getParamWithSearch() queryParam {
+	//now := time.Now()
+	return queryParam{
+		or: map[string]any{
+			"id": "%x%",
+			"\"from\"": "%x%",
+		},
+		expectation: 0,
+	}
+}
+
+func getParamWithState() queryParam {
+	//now := time.Now()
+
+	return queryParam{
+		filter: map[string]any{
+			"state": []string{stateEnum.Failed},
+		},
+		expectation: n >> 1,
 	}
 }
